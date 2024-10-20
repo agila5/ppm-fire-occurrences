f55c37e890a9c63a0f197a4eee2e25208bdcf0b1
Andrea Gilardi <andrea.gilardi@polimi.it>
2024-10-20 19:09:21 +0200

    Added all analyses

--- R/functions.R
+++ R/functions.R
@@ -40,3 +40,251 @@ define_bb <- function(
   }
   st_transform(bbox, 3003)
 }
+
+# 3. Subsets and data transformations -------------------------------------
+tidy_land_use <- function(land_use_raw) {
+  land_use_tidy <- land_use_raw |> 
+    select(Code_18) |> 
+    st_transform(crs = 3003) |> # for spatstat
+    st_set_agr(c(Code_18 = "constant")) |> # remove warning on "st_cast"
+    st_cast("POLYGON") |> 
+    st_make_valid()
+  
+  # Merge together areas with the same macro code
+  land_use_tidy |> 
+    mutate(Code_18 = substr(Code_18, 1, 1))|> # Get the macro code
+    mutate(
+      Code_18 = factor(
+        Code_18, 
+        labels = c(
+          "Artificial surfaces", 
+          "Agricultural areas", 
+          "Forests", 
+          "Water bodies", 
+          "Water bodies"
+        )
+      )
+    ) |> 
+    group_by(Code_18) |> 
+    summarise()
+}
+generate_orig_tif <- function(paths) {
+  temp_folder <- tempdir()
+  unzip(paths, exdir = temp_folder)
+  tif_paths <- list.files(
+    temp_folder, pattern = "\\.tif", recursive = TRUE, 
+    full.names = TRUE
+  )
+  tifs <- lapply(tif_paths, read_stars)
+  do.call(st_mosaic, tifs)
+}
+
+# 5. Modelling ------------------------------------------------------------
+.counting.weights <- function(id, volumes) {
+  id <- as.integer(id)
+  fid <- factor(id, levels = seq_along(volumes))
+  counts <- table(fid)
+  w <- volumes[id] / counts[id]
+  w <- as.vector(w)
+  names(w) <- NULL
+  return(w)
+}
+
+.default.ncube <- function(X){
+  guess.ngrid <- floor((splancs::npts(X) / 2) ^ (1 / 3))
+  max(5, guess.ngrid)
+}
+
+.grid1.index <- function(x, xrange, nx) {
+  i <- ceiling(nx * (x - xrange[1]) / diff(xrange))
+  i <- pmax.int(1, i)
+  i <- pmin.int(i, nx)
+  i
+}
+
+.grid.index <- function(x, y, t, xrange, yrange, trange, nx, ny, nt) {
+  
+  ix <- .grid1.index(x, xrange, nx)
+  iy <- .grid1.index(y, yrange, ny)
+  it <- .grid1.index(t, trange, nt)
+  
+  return(list(ix = ix, iy = iy, it = it, index = as.integer((iy - 1) * nx + ix + (it - 1) * nx * ny)))
+}
+
+stppm <- function(X, formula, dummy_points, dati.interpolati,
+                  ncube = NULL, obsvol, nxyt = NULL, local = FALSE,
+                  verbose = TRUE) {
+  if (!inherits(X, c("stp", "stpm"))) {
+    stop("x should be either of class stp or stpm")
+  }
+
+  time1 <- Sys.time()
+
+
+  if (!is.null(ncube)) {
+    if (!is.numeric(ncube)) {
+      stop("ncube should be a numeric value")
+    } else {
+      if (ncube <= 0) {
+        stop("ncube should be ncube > 0")
+      }
+    }
+  }
+
+  # X è il processo osservato
+
+  X0 <- X
+  X <- X$df
+
+  nX <- nrow(X)
+
+  x <- X[, 1]
+  y <- X[, 2]
+  t <- X[, 3]
+
+  # definire dummy points come un df con x y e t
+
+  quad_p <- rbind(X[, 1:3], dummy_points)
+
+  xx <- quad_p[, 1]
+  xy <- quad_p[, 2]
+  xt <- quad_p[, 3]
+  win <- spatstat.geom::box3(
+    xrange = range(xx), yrange = range(xy),
+    zrange = range(xt)
+  )
+
+  if (is.null(ncube)) {
+    ncube <- .default.ncube(quad_p)
+  }
+  ncube <- rep.int(ncube, 3)
+  nx <- ncube[1]
+  ny <- ncube[2]
+  nt <- ncube[3]
+
+  if (is.null(nxyt)) nxyt <- nx * ny * nt
+  # cubevolume <-  spatstat.geom::volume(win) / nxyt
+  cubevolume <- obsvol / nxyt
+  volumes <- rep.int(cubevolume, nxyt)
+
+  id <- .grid.index(
+    xx, xy, xt, win$xrange, win$yrange, win$zrange,
+    nx, ny, nt
+  )$index
+
+  w <- .counting.weights(id, volumes)
+
+  Wdat <- w[1:nX]
+  Wdum <- w[-c(1:nX)]
+  ndata <- nrow(X)
+  ndummy <- nrow(dummy_points)
+
+  # dati.interpolati sono i dati delle covariate
+
+  z <- c(rep(1, ndata), rep(0, nrow(dummy_points)))
+  y_resp <- z / w
+  dati.cov.marks <- data.frame(cbind(y_resp, w, quad_p, dati.interpolati))
+
+  suppressWarnings(mod_global <- try(gam(
+    as.formula(paste("y_resp",
+      paste(formula, collapse = " "),
+      sep = " "
+    )),
+    weights = w,
+    family = poisson,
+    data = dati.cov.marks
+  ), silent = T))
+  summary(mod_global)
+  res_global <- coef(mod_global)
+  pred_global <- exp(predict(mod_global, newdata = dati.cov.marks[1:nX, ]))
+
+  if (local) {
+    nU <- dim(quad_p)[1]
+    h_x <- MASS::bandwidth.nrd(x)
+    h_y <- MASS::bandwidth.nrd(y)
+    h_t <- MASS::bandwidth.nrd(t)
+    localwt <- matrix(NA, nrow = nX, ncol = nU)
+    if (verbose) {
+      cat(paste(
+        "\n", "Computing Kernel Densities to the",
+        nX, "points", "\n", "\n"
+      ))
+    }
+    for (j in 1:nX) {
+      if (verbose) {
+        spatstat.geom::progressreport(j, nX)
+      }
+      localwt[j, ] <- dnorm(xx - x[j], sd = h_x) *
+        dnorm(xy - y[j], sd = h_y) * dnorm(xt - t[j], sd = h_t)
+    }
+    a_s <- localwt * w
+    res_local <- matrix(NA, nrow = nX, ncol = length(mod_global$coefficients))
+    pred_local <- vector(length = nX)
+    if (verbose) {
+      cat(paste(
+        "\n", "Fitting local model to the", nX, "points",
+        "\n", "\n"
+      ))
+    }
+    for (i in 1:nX) {
+      if (verbose) {
+        spatstat.geom::progressreport(i, nX)
+      }
+      suppressWarnings(mod_local <- try(glm(as.formula(paste("y_resp",
+        paste(formula, collapse = " "),
+        sep = " "
+      )), weights = a_s[i, ], family = poisson, data = dati.cov.marks), silent = T))
+      res_local[i, ] <- mod_local$coefficients
+      pred_local[i] <- exp(predict(mod_local, newdata = dati.cov.marks[i, ]))
+    }
+    res_local <- as.data.frame(res_local)
+    colnames(res_local) <- names(res_global)
+  }
+
+
+
+  time2 <- Sys.time()
+  if (local) {
+    list.obj <- list(
+      IntCoefs = res_global,
+      IntCoefs_local = res_local,
+      X = X0,
+      nX = ndata,
+      I = z,
+      y_resp = y_resp,
+      formula = formula,
+      l = as.vector(pred_global),
+      l_local = as.vector(pred_local),
+      mod_global = mod_global,
+      newdata = dati.cov.marks[1:ndata, ],
+      ncube = ncube,
+      time = paste0(round(as.numeric(difftime(
+        time1 = time2,
+        time2 = time1,
+        units = "sec"
+      )), 3), " sec")
+    )
+    class(list.obj) <- "locstppm"
+  } else {
+    list.obj <- list(
+      IntCoefs = res_global,
+      X = X0,
+      nX = ndata,
+      I = z,
+      y_resp = y_resp,
+      formula = formula,
+      l = as.vector(pred_global),
+      mod_global = mod_global,
+      newdata = dati.cov.marks[1:ndata, ],
+      ncube = ncube,
+      time = paste0(round(as.numeric(difftime(
+        time1 = time2,
+        time2 = time1,
+        units = "sec"
+      )), 3), " sec")
+    )
+    class(list.obj) <- "stppm"
+  }
+  # class(list.obj) <- "stppm"
+  return(list.obj)
+}
--- _targets.R
+++ _targets.R
@@ -22,7 +22,7 @@ pb_download(
   dest = here("data"), 
   repo = "agila5/ppm-fire-occurrences",
   tag = "v1-data", 
-  overwrite = FALSE
+  overwrite = TRUE, 
 )
 
 list(
@@ -166,7 +166,7 @@ list(
       })
       
       st_crs(img_combined) <- "OGC:CRS84"
-      st_warp(img_combined, crs = 3003)
+      img_combined <- st_warp(img_combined, crs = 3003)
       img_combined
     }
   ),
@@ -189,6 +189,340 @@ list(
       # interval). We need to convert it to a 1253 x 1116 x 36 array with 1
       # attribute. See below (NDVI_tidy).
       read_stars(files, quiet = TRUE)
+    }
+  ),
+  # 3. Subsets and data transformations
+  tar_target(
+    name = land_use_tidy_for_spatstat, 
+    command = tidy_land_use(land_use_raw)
+  ), 
+  tar_target(
+    name = land_use_tess, 
+    command = {
+      owins <- 
+        lapply(
+          st_geometry(land_use_tidy_for_spatstat), 
+          as.owin
+        ) |> 
+        set_names(
+          land_use_tidy_for_spatstat[["Code_18"]]
+        )
+      tess(tiles = owins)
+    }
+  ), 
+  tar_target(
+    name = land_use_tidy, 
+    command = {
+      land_use_tidy_for_spatstat |> 
+        st_set_agr(c(Code_18 = "constant")) |> # remove warning on "st_cast"
+        st_cast("POLYGON")
+    }
+  ), 
+  tar_target(
+    name = land_use_tidy_union, 
+    command = {
+      land_use_tidy |> st_geometry() |> st_union() |> st_cast("POLYGON")
+    }
+  ), 
+  tar_target(
+    name = mainland, 
+    command = {
+      idx_mainland <- which.max(st_area(land_use_tidy_union))
+      land_use_tidy_union[idx_mainland]
+    }
+  ), 
+  tar_target(
+    name = fires_italy_ppp, 
+    command = {
+      coords <- fires_italy |> st_geometry() |> as.ppp()
+      setmarks(coords, st_drop_geometry(fires_italy))
+    }
+  ), 
+  tar_target(
+    name = fires_sicily_ppp, 
+    command = {
+      W = Window(land_use_tess)
+      fires_italy_ppp[W]
+    }
+  ), 
+  tar_target(
+    name = fires_sicily_sf, 
+    command = {
+      out <- st_as_sf(fires_sicily_ppp)[-1, ]
+      st_crs(out) <- 3003
+      out
+    }
+  ),
+  tar_target(
+    name = elev, 
+    command = {
+      orig_tif <- generate_orig_tif(elev_files)
+      tif <- orig_tif |> st_downsample(n = c(10, 10)) |> st_warp(crs = 3003)
+      rm(orig_tif); gc()
+      
+      tif <- tif[st_bbox(land_use_tidy_union)]
+      tif[land_use_tidy_union]
+    }
+  ),
+  tar_target(
+    name = slope, 
+    command = {
+      # I cannot save orig_tif in targets format, so I need to repeat the
+      # previous steps
+      orig_tif <- generate_orig_tif(elev_files)
+      
+      # The following is "new"
+      temp_tif <- tempfile(fileext = ".tif")
+      write_stars(orig_tif, temp_tif, progress = FALSE)
+      temp_slope <- tempfile(fileext = ".tif")
+      gdaldem("slope", temp_tif, temp_slope)
+      slope <- read_stars(temp_slope)
+      slope <- slope |> st_downsample(c(10, 10)) |> st_warp(crs = 3003)
+      slope <- slope[st_bbox(land_use_tidy_union)]
+      slope[land_use_tidy_union]
+    }
+  ),
+  tar_target(
+    name = NDVI_tidy, 
+    command = {
+      # The online docs say that the NDVI measurements are shared approximately
+      # every 10 days
+      day_jan <- c(as.Date("2023-01-01"), as.Date("2023-01-11"), as.Date("2023-01-21"))
+      
+      # The following code is taken from the first vignette of stars package. 
+      merge(NDVI_raw) |> 
+        setNames("NDVI") |> 
+        st_set_dimensions(
+          3, 
+          values = c(
+            day_jan, 
+            day_jan + months(1), 
+            day_jan + months(2),
+            day_jan + months(3), 
+            day_jan + months(4),
+            day_jan + months(5), 
+            day_jan + months(6),
+            day_jan + months(7), 
+            day_jan + months(8),
+            day_jan + months(9),
+            day_jan + months(10),
+            day_jan + months(11)
+          ),
+          names = "time"
+        ) |> 
+        st_warp(crs = 3003, cellsize = c(500, 500))
+    }
+  ), 
+  tar_target(
+    name = NDVI_tidy_agg_1month, 
+    command = aggregate(NDVI_tidy, by = "months", FUN = mean)
+  ), 
+  tar_target(
+    name = NDVI_tidy_agg_2months, 
+    command = aggregate(NDVI_tidy, by = "2 months", FUN = mean)
+  ),
+  tar_target(
+    name = NDVI_and_landuse, 
+    command = {
+      NDVI_tidy_new <- st_warp(
+        NDVI_tidy, 
+        st_as_stars(st_bbox(NDVI_tidy), dx = 1500, dy = 1500)
+      )
+      
+      aggregate(
+        NDVI_tidy_new, 
+        by = land_use_tidy_for_spatstat, 
+        FUN = mean, 
+        na.rm = TRUE
+      )
+    }
+  ),
+  tar_target(
+    name = cov_time, 
+    command = {
+      tmp_dir <- tempdir()
+      unzip(env_var_files, exdir = tmp_dir)
+      nc_files <- list.files(
+        paste0(tmp_dir, "/environmental-variables"), 
+        pattern = "\\.nc$", 
+        full.names = TRUE
+      )
+      
+      nome_file <- c(
+        "january.nc", "february.nc", "march.nc", "april.nc",
+        "may.nc", "june.nc", "july.nc",
+        "august.nc", "september.nc", "october.nc", "november.nc", 
+        "december.nc"
+      )
+      
+      lista <- list()
+      for(i in nc_files){
+        k <- which(nome_file == basename(i))
+        suppressMessages({
+          prova <- read_ncdf(i)
+        })
+        st_crs(prova) <- 4326 
+        prova <- st_warp(prova, crs = 3003)
+        prova <- prova[mainland]
+        
+        if (basename(i) == "october.nc"){
+          prova2 <- aggregate(prova[, , , 2, , drop = TRUE], max, by = "1 day")[,-1]  |>
+            st_apply(c("time"), function(z) max(z, na.rm =TRUE))
+        } else {
+          prova2 <- aggregate(prova[, , , ], max, by = "1 day")  |>
+            st_apply(c("time"), function(z) max(z, na.rm =TRUE))
+        }
+        
+        prova2 <- as.data.frame(prova2)
+        prova2$month <- rep(k, nrow(prova2))
+        prova2$day <- 1:nrow(prova2)
+        lista[[k]] <- prova2
+      }
+      
+      do.call(rbind, lista)
+    }
+  ), 
+  
+  # 4. Building the final dataframe that will be used by stopp
+  tar_target(
+    name = stp_time, 
+    command = {normalize(fires_sicily_sf[["ACQ_DATETIME"]], 364)}
+  ), 
+  tar_target(
+    name = stp0, 
+    command = {
+      stp(cbind(spatstat.geom::coords(fires_sicily_ppp) / 1000, stp_time))
+    }
+  ),
+  tar_target(
+    name = dum1, 
+    command = {
+      mainland_coords <- st_coordinates(mainland)
+      set.seed(2)
+      dum1 <- rpp(
+        npoints = npoints(fires_sicily_ppp) * 7, 
+        s.region = mainland_coords[, 1:2] / 1000, 
+        t.region = range(stp_time)
+      )
+      data.frame(x = dum1$xyt[,1], y = dum1$xyt[,2], t =  dum1$xyt[,3])
+    }
+  ), 
+  tar_target(
+    name = covs0_spatial, 
+    command = {
+      elev_im <- spatstat.geom::rescale(as.im(elev), 1000)
+      slope_im <- spatstat.geom::rescale(as.im(slope), 1000)
+      land_use_f <- as.function(land_use_tess)
+      
+      covs0_spatial <- rbind(
+        cbind(
+          land_use_f(stp0$df$x * 1000, stp0$df$y * 1000), 
+          slope_im[list(x = stp0$df$x, y = stp0$df$y), drop = FALSE], 
+          elev_im[list(x = stp0$df$x, y = stp0$df$y), drop = FALSE] 
+        ), 
+        cbind(
+          land_use_f(dum1$x * 1000, dum1$y * 1000), 
+          slope_im[list(x = dum1$x, y = dum1$y), drop = FALSE], 
+          elev_im[list(x = dum1$x, y = dum1$y), drop = FALSE]
+        )
+      )
+      colnames(covs0_spatial) <- c("land", "slope", "elev")
+      covs0_spatial
+    }
+  ), 
+  tar_target(
+    name = pts_combined, 
+    command = {
+      pts_combined <- rbind(stp0$df, dum1) |> setNames(c("x", "y", "time"))
+      pts_combined[, c("x", "y")] <- pts_combined[, c("x", "y")] * 1000 # Since we previously converted m to km
+      pts_combined <- st_as_sf(pts_combined, coords = c("x", "y"), crs = 3003)
+      pts_combined$time <- (as.Date("2023-01-01") + pts_combined$time) |> round_date(unit = "day")
+      pts_combined
+    }
+  ),
+  tar_target(
+    name = env_cov_st, 
+    command = {
+      out <- st_extract(
+        x = env_var_agg_1day,
+        at = pts_combined,
+        time_column = "time"
+      )
+      st_drop_geometry(out)[, -c(12)]
+    }
+  ), 
+  tar_target(
+    name = NDVI_cov_st, 
+    command = {
+      st_extract(
+        x = NDVI_tidy,
+        at = pts_combined |> mutate(time = round_date(time, "month")),
+        time_column = "time", 
+      )
+    }
+  ), 
+  tar_target(
+    name = covs_complete, 
+    command = {
+      out <- cbind(
+        x = c(stp0$df$x, dum1$x), 
+        y = c(stp0$df$y, dum1$y), 
+        t = c(stp0$df$t, dum1$t), 
+        covs0_spatial, 
+        env_cov_st, 
+        NDVI = NDVI_cov_st$NDVI 
+      )
+      # Rescale elev from m to km
+      out$elev <- out$elev / 1000
+      out
+    }
+  ), 
+  
+  # 5. Modelling
+  tar_target(
+    name = volume0, 
+    command = {
+      area(
+        Window(fires_sicily_ppp |> spatstat.geom::rescale(1000))
+      ) * abs(diff(range(stp0$df$t)))
+    }
+  ), 
+  tar_target(
+    name = mod_global, 
+    command = {
+      # Please notice that stppm is defined in R/functions.R
+      stppm(
+        X = stp0, 
+        formula = ~
+          as.factor(land) + NDVI + 
+          + elev + slope 
+        + u10 + stl2 
+        + tp
+        , 
+        dummy_points = dum1, 
+        dati.interpolati = covs_complete[3 + c(1, 2, 3, 4, 10, 13, 14, 15)], 
+        obsvol = volume0,  ncube = 5, nxyt = 87
+      )
+    }
+  ),
+  tar_target(
+    name = mod_local, 
+    command = {
+      # Please notice that stppm is defined in R/functions.R
+      stppm(
+        X = stp0, 
+        formula = ~
+          as.factor(land) + NDVI + 
+          + elev + slope 
+        + u10 + stl2 
+        + tp
+        , 
+        dummy_points = dum1, 
+        dati.interpolati = covs_complete[3 + c(1, 2, 3, 4, 10, 13, 14, 15)], 
+        obsvol = volume0,  ncube = 5, nxyt = 87, 
+        local = TRUE, 
+        verbose = TRUE
+      )
     }
   ),
   
